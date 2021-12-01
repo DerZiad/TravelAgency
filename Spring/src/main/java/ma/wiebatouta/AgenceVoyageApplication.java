46e38b6664ff95f8585b6dca34f9414fd3001971
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2021-12-01 12:25:06 +0100

    ZIad
    POSH

---
+++ Spring/src/main/java/ma/wiebatouta/AgenceVoyageApplication.java
@@ -0,0 +1,13 @@
+package ma.wiebatouta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceVoyageApplication.class, args);
+	}
+
+}
