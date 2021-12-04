39a353a063bfde6e7c75359f3b6b45667d30744d
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2021-12-04 09:42:50 +0100

    Continue ayman
    J'ai ajouter reservation

--- Spring/src/main/java/ma/wiebatouta/models/Reservation.java
+++ Spring/src/main/java/ma/wiebatouta/models/Reservation.java
@@ -1,5 +1,53 @@
 package ma.wiebatouta.models;
+/**
+ * 
+ */
+import java.io.Serializable;
 
-public class Reservation {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.wiebatouta.models.idcompose.KeyReservation;
+
+@Entity
+@Table(name="reservations")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Reservation implements Serializable,Comparable<Reservation>,Statistique{
+	
+	private static final long serialVersionUID = 1L;
+
+
+	private KeyReservation id = new KeyReservation();
+	
+	
+	boolean isConfirmed = false;
+	boolean isReserved = false;
+	
+	/**
+	 * Relations
+	 * */
+	
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST })
+	@MapsId("idVoyage")
+	private Voyage product;
+	
+	
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST })
+	@MapsId("idPerson")
+	private Personne person;
+	
+	
+	@Override
+	public int compareTo(Reservation o) {
+		return id.compareTo(o.getId());
+	}
 
 }
