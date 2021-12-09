3b855e38bf17d0ef82c38219a05de8baee779c10
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2021-12-09 09:53:22 +0100

    New changes
    Hello

--- Spring/src/main/java/ma/wiebatouta/controllers/AuthenticationController.java
+++ Spring/src/main/java/ma/wiebatouta/controllers/AuthenticationController.java
@@ -1,7 +1,10 @@
 package ma.wiebatouta.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,11 +12,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ma.wiebatouta.models.Equipe;
+import ma.wiebatouta.models.Hotel;
+import ma.wiebatouta.models.Reservation;
+import ma.wiebatouta.models.Voyage;
+import ma.wiebatouta.repositories.EquipeRepository;
+import ma.wiebatouta.repositories.HotelRepository;
+import ma.wiebatouta.repositories.ReservationRepository;
+import ma.wiebatouta.repositories.VoyageRepository;
+
 @Controller
 public class AuthenticationController {
 
+	@Autowired private VoyageRepository voyageRepository;
+	@Autowired private HotelRepository hotelRepository;
+	@Autowired private ReservationRepository reservationRepository;
+	@Autowired private EquipeRepository equipeRepository;
+	
+	
+	
 	private static final String PATH_ADMIN_LOGIN = "login";
-
+	private static final String PATH_ADMIN_PAGE = "index-a";
+	
 	private static final String ATTRIBUT_ERROR = "error";
 	private static final String REDIRECT_ADMIN_SPACE = "redirect:/admin/";
 
@@ -31,5 +51,14 @@ public class AuthenticationController {
 			}
 		}
 		return model;
+	}
+	
+	@GetMapping("/admin/")
+	public ModelAndView getIndexAdmin() {
+		List<Voyage> voyages = voyageRepository.findAll();
+		List<Hotel> hotels = hotelRepository.findAll();
+		List<Reservation> reservations = reservationRepository.findAll();
+		List<Equipe> equipes = equipeRepository.findAll();
+		return new ModelAndView(PATH_ADMIN_PAGE);
 	}
 }
