6a2a7441a219ceb76793245a252b80515852c2f3
ABOUHALI <abouhaliayman3@gmail.com>
2021-12-11 22:05:21 +0100

    ss
    ss

--- Spring/src/main/java/ma/wiebatouta/controllers/LieuController.java
+++ Spring/src/main/java/ma/wiebatouta/controllers/LieuController.java
@@ -1,41 +1,43 @@
-package ma.wiebatouta.restcontroller;
+package ma.wiebatouta.controllers;
 
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import ma.wiebatouta.controllers.DesignAttributes;
 
 import ma.wiebatouta.metier.LieuxMetier;
 import ma.wiebatouta.models.Lieu;
 
-@RestController
-@RequestMapping("/api/lieux")
-public class LieuxRestController {
+@Controller
+@RequestMapping("/lieux")
+public class LieuController {
 	@Autowired
 	private LieuxMetier lm ;
-	
-	/*@GetMapping
+	private final static String PATH_HOTEL = "lieu/addLieu";
+
+	@GetMapping
 	@RolesAllowed("ADMIN")
 	public ModelAndView listeLieux(){
-		ModelAndView model = new ModelAndView("/hotel/index");
+		ModelAndView model = new ModelAndView(PATH_HOTEL);
 		List<Lieu> lieux = lm.listeLieux();
 		model.addObject("lieux", lieux);
+		model.addObject(DesignAttributes.ACTIVE_HOTEL,DesignAttributes.ACTIVE);
+
 		return model;
 		
-	}*/
+	}
 	
-	@PostMapping
+	/*@PostMapping
 	@RolesAllowed("ADMIN")
 	public Lieu saveLieu(@RequestBody Lieu l) {
 		return lm.save(l);
-	}
-	
-	
+	}*/
 }
